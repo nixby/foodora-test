c50d2648d89b3b66d87a35328a3ef53ff5ecb76e
Polina Belonozhka <belonozhka.polina@gmail.com>
2016-06-15 01:27:58 +0200

    Add test DB configuration

---
+++ src/DBConfig.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FoodoraTest;
+
+/**
+ * DBConfig to save connection information for the database
+ *
+ * @package  FoodoraTest
+ * @version   0.0.1
+ * @license  MIT
+ *
+ */
+class DBConfig
+{
+    /**
+     * @var string HOST hostname of the database
+     */
+    const HOST = 'localhost';
+
+    /**
+     * @var string NAME database name
+     */
+    const NAME = 'foodora-test';
+
+    /**
+     * @var string USER username to connect with
+     */
+    const USER = 'food';
+
+    /**
+     * @var string PASSWORD password to the above mentioned user
+     */
+    const PASSWORD = 'F00dora15!';
+}
